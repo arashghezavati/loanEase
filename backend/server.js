20d6db9546c9796a665cd9a03b4843ac822a93ba
Arash <76617498+arashghezavati@users.noreply.github.com>
2023-12-06 22:13:24 -0800

    second update on server .js

--- backend/server.js
+++ backend/server.js
@@ -20,13 +20,7 @@ const allowedOrigins = [
 ];
 
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: allowedOrigins,
 };
 
 app.use(cors(corsOptions));
